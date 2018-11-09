dad41d3698cb3ee9ebc5b7e13e066fbfe0c1a5b5
Jay Jackson <jaynickjackson@gmail.com>
2018-11-08 21:18:11 -0500

    add googlemap script, createMap function, some styles

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-alert('javascript is working')
+window.init = _ => {
+    console.log('google maps loaded');
+    let map;
+
+    createMap({});
+
+    function createMap ({latitude = 39.76838, longitude = -86.15804}) {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {
+                lat: latitude,
+                lng: longitude
+            },
+            zoom: 13,
+            disableDefaultUI: true,
+        });
+    }
+}
