28a7adde9e8d4a072a8b2da4335e574eff19e7bb
Jay Jackson <jaynickjackson@gmail.com>
2018-11-08 21:55:21 -0500

    add part-2 hello map

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ window.init = _ => {
     console.log('google maps loaded');
     let map;
 
-    createMap({});
+    getUserLocation().then(
+        ({ coords }) => createMap(coords), // resolve
+        err => console.error('This browser does not support geolocation: ', err) // reject
+    )
+
+    function getUserLocation () {
+        return new Promise((res, rej) => {
+            navigator.geolocation.getCurrentPosition(res, rej);
+        });
+    }
 
     function createMap ({latitude = 39.76838, longitude = -86.15804}) {
         map = new google.maps.Map(document.getElementById('map'), {
